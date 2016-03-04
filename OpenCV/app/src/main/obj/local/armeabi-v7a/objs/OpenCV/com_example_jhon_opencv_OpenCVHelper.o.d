7f20632641b2ba1e8411364b9342be39342838f2
YRaining <1003736745@163.com>
2016-03-04 18:11:46 +0800

    Added files via upload

---
+++ OpenCV/app/src/main/obj/local/armeabi-v7a/objs/OpenCV/com_example_jhon_opencv_OpenCVHelper.o.d
@@ -0,0 +1,281 @@
+C:/Users/Jhon/AndroidStudioProjects/OpenCV/app/src/main/obj/local/armeabi-v7a/objs/OpenCV/com_example_jhon_opencv_OpenCVHelper.o: \
+ C:/Users/Jhon/AndroidStudioProjects/OpenCV/app/src/main/jni/com_example_jhon_opencv_OpenCVHelper.cpp \
+ C:/Users/Jhon/AndroidStudioProjects/OpenCV/app/src/main/jni/com_example_jhon_opencv_OpenCVHelper.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/jni.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/android/api-level.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/stdio.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/stdint.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/_types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/_types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/asm/types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/stdlib.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/string.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/malloc.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/alloca.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/strings.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/memory.h \
+ ../../../../native/jni/include/opencv2/opencv.hpp \
+ ../../../../native/jni/include/opencv2/core.hpp \
+ ../../../../native/jni/include/opencv2/core/cvdef.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/limits.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/limits.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/limits.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/internal_types.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/limits.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/asm/page.h \
+ ../../../../native/jni/include/opencv2/core/hal/interface.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdint \
+ ../../../../native/jni/include/opencv2/core/version.hpp \
+ ../../../../native/jni/include/opencv2/core/base.hpp \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/climits \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ ../../../../native/jni/include/opencv2/core/cvstd.hpp \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ ../../../../native/jni/include/opencv2/core/ptr.inl.hpp \
+ ../../../../native/jni/include/opencv2/core/neon_utils.hpp \
+ ../../../../native/jni/include/opencv2/core/traits.hpp \
+ ../../../../native/jni/include/opencv2/core/matx.hpp \
+ ../../../../native/jni/include/opencv2/core/saturate.hpp \
+ ../../../../native/jni/include/opencv2/core/fast_math.hpp \
+ ../../../../native/jni/include/opencv2/core/types.hpp \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat \
+ ../../../../native/jni/include/opencv2/core/mat.hpp \
+ ../../../../native/jni/include/opencv2/core/bufferpool.hpp \
+ ../../../../native/jni/include/opencv2/core/mat.inl.hpp \
+ ../../../../native/jni/include/opencv2/core/persistence.hpp \
+ ../../../../native/jni/include/opencv2/core/operations.hpp \
+ ../../../../native/jni/include/opencv2/core/cvstd.inl.hpp \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ ../../../../native/jni/include/opencv2/core/utility.hpp \
+ ../../../../native/jni/include/opencv2/core/core_c.h \
+ ../../../../native/jni/include/opencv2/core/types_c.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/assert.h \
+ ../../../../native/jni/include/opencv2/core/optim.hpp \
+ ../../../../native/jni/include/opencv2/imgproc.hpp \
+ ../../../../native/jni/include/opencv2/imgproc/imgproc_c.h \
+ ../../../../native/jni/include/opencv2/imgproc/types_c.h \
+ ../../../../native/jni/include/opencv2/photo.hpp \
+ ../../../../native/jni/include/opencv2/photo/photo_c.h \
+ ../../../../native/jni/include/opencv2/video.hpp \
+ ../../../../native/jni/include/opencv2/video/tracking.hpp \
+ ../../../../native/jni/include/opencv2/video/background_segm.hpp \
+ ../../../../native/jni/include/opencv2/video/tracking_c.h \
+ ../../../../native/jni/include/opencv2/features2d.hpp \
+ ../../../../native/jni/include/opencv2/flann/miniflann.hpp \
+ ../../../../native/jni/include/opencv2/flann/defines.h \
+ ../../../../native/jni/include/opencv2/flann/config.h \
+ ../../../../native/jni/include/opencv2/objdetect.hpp \
+ ../../../../native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ ../../../../native/jni/include/opencv2/objdetect/objdetect_c.h \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ ../../../../native/jni/include/opencv2/calib3d.hpp \
+ ../../../../native/jni/include/opencv2/core/affine.hpp \
+ ../../../../native/jni/include/opencv2/calib3d/calib3d_c.h \
+ ../../../../native/jni/include/opencv2/imgcodecs.hpp \
+ ../../../../native/jni/include/opencv2/videoio.hpp \
+ ../../../../native/jni/include/opencv2/highgui.hpp \
+ ../../../../native/jni/include/opencv2/highgui/highgui_c.h \
+ ../../../../native/jni/include/opencv2/imgcodecs/imgcodecs_c.h \
+ ../../../../native/jni/include/opencv2/videoio/videoio_c.h \
+ ../../../../native/jni/include/opencv2/ml.hpp \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
+ C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/fstream
+
+C:/Users/Jhon/AndroidStudioProjects/OpenCV/app/src/main/jni/com_example_jhon_opencv_OpenCVHelper.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/jni.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/stdio.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/stdint.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/asm/types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/stdlib.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/string.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/malloc.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/alloca.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/strings.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/memory.h:
+
+../../../../native/jni/include/opencv2/opencv.hpp:
+
+../../../../native/jni/include/opencv2/core.hpp:
+
+../../../../native/jni/include/opencv2/core/cvdef.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/limits.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/machine/limits.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/asm/page.h:
+
+../../../../native/jni/include/opencv2/core/hal/interface.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdint:
+
+../../../../native/jni/include/opencv2/core/version.hpp:
+
+../../../../native/jni/include/opencv2/core/base.hpp:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/climits:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+../../../../native/jni/include/opencv2/core/cvstd.hpp:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+../../../../native/jni/include/opencv2/core/ptr.inl.hpp:
+
+../../../../native/jni/include/opencv2/core/neon_utils.hpp:
+
+../../../../native/jni/include/opencv2/core/traits.hpp:
+
+../../../../native/jni/include/opencv2/core/matx.hpp:
+
+../../../../native/jni/include/opencv2/core/saturate.hpp:
+
+../../../../native/jni/include/opencv2/core/fast_math.hpp:
+
+../../../../native/jni/include/opencv2/core/types.hpp:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat:
+
+../../../../native/jni/include/opencv2/core/mat.hpp:
+
+../../../../native/jni/include/opencv2/core/bufferpool.hpp:
+
+../../../../native/jni/include/opencv2/core/mat.inl.hpp:
+
+../../../../native/jni/include/opencv2/core/persistence.hpp:
+
+../../../../native/jni/include/opencv2/core/operations.hpp:
+
+../../../../native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+../../../../native/jni/include/opencv2/core/utility.hpp:
+
+../../../../native/jni/include/opencv2/core/core_c.h:
+
+../../../../native/jni/include/opencv2/core/types_c.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/platforms/android-15/arch-arm/usr/include/assert.h:
+
+../../../../native/jni/include/opencv2/core/optim.hpp:
+
+../../../../native/jni/include/opencv2/imgproc.hpp:
+
+../../../../native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+../../../../native/jni/include/opencv2/imgproc/types_c.h:
+
+../../../../native/jni/include/opencv2/photo.hpp:
+
+../../../../native/jni/include/opencv2/photo/photo_c.h:
+
+../../../../native/jni/include/opencv2/video.hpp:
+
+../../../../native/jni/include/opencv2/video/tracking.hpp:
+
+../../../../native/jni/include/opencv2/video/background_segm.hpp:
+
+../../../../native/jni/include/opencv2/video/tracking_c.h:
+
+../../../../native/jni/include/opencv2/features2d.hpp:
+
+../../../../native/jni/include/opencv2/flann/miniflann.hpp:
+
+../../../../native/jni/include/opencv2/flann/defines.h:
+
+../../../../native/jni/include/opencv2/flann/config.h:
+
+../../../../native/jni/include/opencv2/objdetect.hpp:
+
+../../../../native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+../../../../native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+../../../../native/jni/include/opencv2/calib3d.hpp:
+
+../../../../native/jni/include/opencv2/core/affine.hpp:
+
+../../../../native/jni/include/opencv2/calib3d/calib3d_c.h:
+
+../../../../native/jni/include/opencv2/imgcodecs.hpp:
+
+../../../../native/jni/include/opencv2/videoio.hpp:
+
+../../../../native/jni/include/opencv2/highgui.hpp:
+
+../../../../native/jni/include/opencv2/highgui/highgui_c.h:
+
+../../../../native/jni/include/opencv2/imgcodecs/imgcodecs_c.h:
+
+../../../../native/jni/include/opencv2/videoio/videoio_c.h:
+
+../../../../native/jni/include/opencv2/ml.hpp:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+
+C:/Users/Jhon/Downloads/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/fstream:
